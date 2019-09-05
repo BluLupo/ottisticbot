56d57f1ff0bb9dfe6395c51a340a9b14fb945157
Hersel Giannella <hersel1991@gmail.com>
2019-09-05 17:24:26 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+bot_token = "INSERT TOKEN HERE"
+channel_id = -123456789 # private channel for reports
+#DATABASE MARIADB
+database = {
+  'server': 'localhost',
+  'user': 'user',
+  'password': 'password',
+  'name': 'dbname',
+}
